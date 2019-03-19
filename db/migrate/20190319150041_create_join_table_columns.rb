0ca60151837dff7781dbfe137345e2e741c60990
github username <github email address>
2019-03-19 11:07:52 -0400

    Done.

---
+++ db/migrate/20190319150041_create_join_table_columns.rb
@@ -0,0 +1,5 @@
+class CreateJoinTableColumns < ActiveRecord::Migration[5.0]
+  def change
+    add_column :rides, :passenger_id, :integer
+  end
+end
---
+++ db/migrate/20190319150711_add_to_join_table.rb
@@ -0,0 +1,5 @@
+class AddToJoinTable < ActiveRecord::Migration[5.0]
+  def change
+    add_column :rides, :taxi_id, :integer
+  end
+end
